9748cd6ba2cba4a238b260ee23a4b6653b28945d
JonasButikofer <brojobutikofer@gmail.com>
2025-03-27 22:22:42 -0600

    final update with bootstrap

--- frontend/ProjectFrontend/src/components/PageHeader.tsx
+++ frontend/ProjectFrontend/src/components/PageHeader.tsx
@@ -1,12 +1,28 @@
-import "./PageHeader.css";
+import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function PageHeader() {
   return (
-    <header className="page-header">
-      <div className="header-content">
-        <h1 className="page-title">Book Store</h1>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">Book Store</Link>
+
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to="/">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/cart">Cart</Link>
+            </li>
+          </ul>
+        </div>
       </div>
-    </header>
+    </nav>
   );
 }
 
